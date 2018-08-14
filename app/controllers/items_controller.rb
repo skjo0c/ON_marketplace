20a4bbae4307320fdc2124352724a05cc12d772b
skj <skj.jo0c@gmail.com>
2018-08-14 17:11:02 +0545

    first commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,29 @@
+class ItemsController < ApplicationController
+
+	before_action :authenticate_admin!, only: [:create, :delete]
+
+	def index
+		@items = Item.all
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item = Item.create(item_params)
+
+		if @item.valid?
+			redirect_to_root_path
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+	private
+
+	def item_params
+		params.require(:item).permit(:name, :image, :price, :description)
+	end
+	
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+	root 'items#index'
+	resources :items
+  devise_for :admins
+  devise_for :users
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
